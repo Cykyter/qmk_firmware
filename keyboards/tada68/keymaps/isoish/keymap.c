1831598e26a38ea119ea2196698335d21a859e52
Pentti Laitinen <cykyter@gmail.com>
2017-10-29 19:02:18 +0200

    Edited ISO layout for Tada68
    - Removed unused code.
    - Added basic readme.

--- keyboards/tada68/keymaps/isoish/keymap.c
+++ keyboards/tada68/keymaps/isoish/keymap.c
@@ -1,13 +1,5 @@
 #include "tada68.h"
 
-
-// Used for SHIFT_ESC
-#define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
-
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
 #define _BL 0
 #define _FL1 1
 #define _FL2 2
@@ -54,59 +46,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,_______,_______,_______,_______,_______,_______,_______,_______,KC_MPRV,KC_MPLY,KC_MNXT,_______,_______,KC_HOME, \
   _______,_______,_______,_______,_______,_______,_______,_______,KC_VOLD,KC_MUTE,KC_VOLU,_______,_______,KC_END, \
   _______,_______,_______,                 _______,               _______,_______,_______,_______,_______, _______),
-
-  /* Keymap _FL2: Function Layer 2
-   * ,----------------------------------------------------------------.
-   * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|  RESET|PSCR|
-   * |----------------------------------------------------------------|
-   * |     |   |   |   |   |   |   |   |   |   |   |BL-|BL+|BL   | INS|
-   * |----------------------------------------------------------------|
-   * |      |   |   |   |   |   |   |   |   | PP|PLA| PN|        |HOME|
-   * |----------------------------------------------------------------|
-   * |        |   |   |   |   |   |   |   |  V-| MV| V+|     |   | END|
-   * |----------------------------------------------------------------|
-   * |    |    |    |                       |   |   |    |   |   |    |
-   * `----------------------------------------------------------------'
-   */
-
-  [_FL2] = KEYMAP_ISO(
-  _______, _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,RESET,_______,  \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, BL_DEC,BL_INC, BL_TOGG,_______, \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
-  _______,_______,_______,                 _______,               _______,_______,_______,_______,_______, _______),
 };
-
-/*enum function_id {
-    //SHIFT_ESC,
-};
-
-const uint16_t PROGMEM fn_actions[] = {
-  //[0]  = ACTION_FUNCTION(SHIFT_ESC),
-};
-
-void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
-  static uint8_t shift_esc_shift_mask;
-  switch (id) {
-    case SHIFT_ESC:
-      shift_esc_shift_mask = get_mods()&MODS_CTRL_MASK;
-      if (record->event.pressed) {
-        if (shift_esc_shift_mask) {
-          add_key(KC_GRV);
-          send_keyboard_report();
-        } else {
-          add_key(KC_ESC);
-          send_keyboard_report();
-        }
-      } else {
-        if (shift_esc_shift_mask) {
-          del_key(KC_GRV);
-          send_keyboard_report();
-        } else {
-          del_key(KC_ESC);
-          send_keyboard_report();
-        }
-      }
-      break;
-  }
-}*/
